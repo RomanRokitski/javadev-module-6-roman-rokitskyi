06928607526b46ae0078362091316e9299bc148c
RomanRokitski <romanrokitski@gmail.com>
2023-05-06 14:57:13 +0300

    first commit

--- src/main/java/org/example/conf/Database.java
+++ src/main/java/org/example/conf/Database.java
@@ -1,25 +1,25 @@
 package org.example.databaseUtils;
 
-import org.h2.Driver;
+import org.example.conf.FlywayConfigurations;import org.h2.Driver;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
-    private static final String H2_URL = "jdbc:h2:~/H2/homework3";
-    private static final String USERNAME = "roman";
-    private static final String PASSWORD = "123";
+    private static final String FLYWAY_CONNECTION_URL = "jdbc:h2:~/H2/homework3";
+    private static final String FLYWAY_USER = "roman";
+    private static final String FLYWAY_PASSWORD = "123";
     private final Connection connection;
 
     public Database() throws SQLException {
         DriverManager.registerDriver(new Driver());
-        connection = DriverManager.getConnection(H2_URL, USERNAME, PASSWORD);
+        connection = DriverManager.getConnection(FLYWAY_CONNECTION_URL, FLYWAY_USER, FLYWAY_PASSWORD);
     }
 
-    public static Database getInstance() {
+    public static FlywayConfigurations.Database getInstance() {
         try {
-            return new Database();
+            return new FlywayConfigurations.Database();
         } catch (Exception e) {
             throw new RuntimeException("Instantiation od Database failed", e);
         }
--- src/main/java/org/example/sql/find_max_salary_worker.sql
+++
@@ -1,6 +0,0 @@
-SELECT name, salary
-FROM worker
-INNER JOIN (
-  SELECT MAX(salary) AS max_salary
-  FROM worker
-) ON salary = max_salary;
